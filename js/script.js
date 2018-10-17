b5ebd5f0c452b8fac1fe23f86f0b1d22234ed077
zsalec <zs_alec@163.com>
2018-10-17 15:42:41 +0800

    'day1'

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+
+	$(".close-alert").click(() => {
+		$(".alert").alert("close");
+	});
+
+	$('.alert').on('close.bs.alert', () => {
+		console.log('close alert');
+	});
+
+	$('.alert').on('closed.bs.alert', () => {
+		console.log('closed alert');
+	});
+});
